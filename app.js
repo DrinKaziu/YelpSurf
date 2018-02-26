a54dc8791c33454dd30a0da28caa63497b23a1df
Drin Kaziu <dak4410@gmail.com>
2018-02-26 00:46:20 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var     surfspotRoutes = require("./routes/surfspots"),
         commentsRoutes = require("./routes/comments"),
         indexRoutes     = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_surf");
+// mongoose.connect("mongodb://localhost/yelp_surf");
+mongoose.connect("mongodb://SnowSurfer:Incantesimo1!@ds249398.mlab.com:49398/yelpsurf");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
